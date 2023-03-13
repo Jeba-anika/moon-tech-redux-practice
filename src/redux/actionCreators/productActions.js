e6ff09a2c55947560ec3e2ecc09f9e8e0cc4d5a4
Jeba-anika <anikanur98@gmail.com>
2023-03-13 22:37:44 +0600

    added redux to the project

---
+++ src/redux/actionCreators/productActions.js
@@ -0,0 +1,19 @@
+import actionTypes from "../actionTypes/actionTypes"
+
+export const addToCart = (data)=>{
+    return {
+        type: actionTypes.ADD_TO_CART,
+        payload: data
+    }
+
+}
+
+
+export const removeFromCart = (data)=>{
+    return {
+        type: actionTypes.REMOVE_FROM_CART,
+        payload: data
+    }
+
+}
+
